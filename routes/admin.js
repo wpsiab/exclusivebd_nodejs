0f58da0c29fa752c2bf6a01321507bfb99b4e9a7
Md.Abdullah <abdullah.wpsi@gmail.com>
2019-03-15 01:55:08 +0600

    first commit

---
+++ routes/admin.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+
+const router = express.Router();
+//admin add product =>get
+router.get('/add-product', (req, res, next) => {
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+});
+//admin add product post
+router.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+module.exports = router;
